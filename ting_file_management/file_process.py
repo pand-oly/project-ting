5b1cd7351b6242e086954e874c237d56e37483be
pand-oly <hoa98hoa@gmail.com>
2023-02-04 20:02:26 -0300

    feat(requirement 3):

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
-def process(path_file, instance):
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
+
+
+def process(path_file: str, instance: Queue):
     """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+
+    importer = txt_importer(path_file)
+    result_file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(importer),
+        "linhas_do_arquivo": importer,
+    }
+
+    instance.enqueue(result_file_dict)
+    return sys.stdout.write(str(result_file_dict))
 
 
 def remove(instance):
